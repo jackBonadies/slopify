f0729c6d4621fe4b27d13f82e9c1287a1df62b5f
jackBonadies <jbonadies6@gmail.com>
2019-03-16 13:58:35 -0400

    embedded osascript into python using 'subprocess', 'popen', etc

--- slopify.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ARTIST=`osascript -e 'tell application "Spotify" to artist of current track as string'`
-TRACK=`osascript -e 'tell application "Spotify" to name of current track as string'`
-echo "$TRACK by $ARTIST lyrics"
-curl -# -L https://www.lyrics.com/lyric/32840200/$ARTIST/$TRACK -o temp.txt 
-echo "--------------------------"
-./regexPy.py
