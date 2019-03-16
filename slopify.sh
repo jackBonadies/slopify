4f67e7edb4d7c7dfae2893d2dabdc180e81af76e
jackBonadies <jbonadies6@gmail.com>
2019-03-16 13:56:59 -0400

    used request instead of cli alternative like curl or wget.  used actual html parser rather than regex.

--- slopify.sh
+++ slopify.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 ARTIST=`osascript -e 'tell application "Spotify" to artist of current track as string'`
 TRACK=`osascript -e 'tell application "Spotify" to name of current track as string'`
-echo "$TRACK by $ARTIST genius lyrics"
-curl -# -L https://www.azlyrics.com/lyrics/anohni/4degrees.html -o temp.txt 
+echo "$TRACK by $ARTIST lyrics"
+curl -# -L https://www.lyrics.com/lyric/32840200/$ARTIST/$TRACK -o temp.txt 
+echo "--------------------------"
+./regexPy.py
