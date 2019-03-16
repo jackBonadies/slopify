304112a3f53d13229f8364dc4956c51c16107656
jackBonadies <jbonadies6@gmail.com>
2019-03-15 23:51:25 -0400

    version 1 regex machine

--- slopify.sh
+++ slopify.sh
@@ -2,4 +2,4 @@
 ARTIST=`osascript -e 'tell application "Spotify" to artist of current track as string'`
 TRACK=`osascript -e 'tell application "Spotify" to name of current track as string'`
 echo "$TRACK by $ARTIST genius lyrics"
-curl -# -L https://genius.com/Anohni-why-did-you-separate-me-from-the-earth-lyrics | grep "<div class=\"lyrics\">"
+curl -# -L https://www.azlyrics.com/lyrics/anohni/4degrees.html -o temp.txt 
