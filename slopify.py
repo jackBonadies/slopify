66827ac790e20f41c6a700046ef5e548a780c9cd
jackBonadies <jbonadies6@gmail.com>
2019-03-23 19:05:47 -0400

    lyrics app works.  only thing to improve is GUI

--- slopify.py
+++ slopify.py
@@ -3,7 +3,8 @@ from bs4 import BeautifulSoup
 import requests
 import os
 import subprocess
-
+from tkinter import *
+from tkinter import scrolledtext
 def getArtist():
 	script = 'tell application "Spotify" to artist of current track as string' 
 	p = subprocess.Popen(['osascript', '-'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
@@ -34,11 +35,21 @@ def getLyricsFromLink(url):
   src = requests.get(url).text
   soup = BeautifulSoup(src,'lxml')
   lyrics = soup.find('p')
-  print(lyrics.text)
+  return lyrics.text
+def gui(lyrics):
+  window = Tk()
+  window.title('slopify')
+  window.geometry('350x600')
+  unit = scrolledtext.ScrolledText(window,width=300,height=550) 
+  unit.grid(column=0,row=0)
+  unit.insert(INSERT,lyrics)
+  window.mainloop()
 artist=getArtist()
 title=getTrack()
 src = searchGoogle(getArtist(),getTrack())
 url = getFirstLinkFromGoogle(src)
 #p = subprocess.Popen(['clear'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
 #p.communicate()
-getLyricsFromLink(url)
+lyrics = getLyricsFromLink(url)
+#gui(lyrics)
+print(lyrics)
